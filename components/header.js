7eb95a4db498ae19fa8aa2fd2f463a39c6a2481d
Air2air <todd@dunningmarketing.com>
2021-07-03 18:46:18 -0700

    Header

---
+++ components/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "../styles/Header.module.scss";
+
+export default function Header() {
+  return (
+    <div>
+      <h1 className={styles.title}>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+
+      <p className={styles.description}>
+        Get started by editing{" "}
+        <code className={styles.code}>pages/index.js</code>
+      </p>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Header from './../components/header'
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
@@ -11,15 +12,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Header />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <main className={styles.main}>
+
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
