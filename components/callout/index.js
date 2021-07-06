1cc5d5220dd0a8cec806227c0c3eea93606a36e8
Air2air <todd@dunningmarketing.com>
2021-07-05 20:40:55 -0700

    Callout

--- components/callout/index.js
+++ components/callout/index.js
@@ -1,21 +1,41 @@
 import React, { useState, useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
 import { TextLeft, TextRight } from "./text/text";
 import Companies from "./companies/companies";
 import People from "./people/people";
 import styles from "./Callout.module.scss";
 
+
 const Callout = () => {
+  const animateCalloutLeft = () => ({
+    translateX: -600,
+    transition: { duration: 0.8, delay: 6, ease: "backInOut" },
+  });
+
+  const animateCalloutRight = () => ({
+    translateX: 600,
+    transition: { duration: 0.8, delay: 6, ease: "backInOut" },
+  });
+
   return (
     <>
       <div className="d-flex justify-content-around flex-wrap">
-        <div className="d-flex flex-column align-items-center w-50">
-          <TextLeft delayWrapperLeft={3} delayWord1={1} delayWord2={1.5} />
+        <motion.div
+          initial={{ translateX: 0 }}
+          animate={animateCalloutLeft}
+          className="d-flex flex-column align-items-center w-50"
+        >
+          <TextLeft animateCalloutLeft={3} delayWord1={1} delayWord2={1.5} />
           <People delay={1} />
-        </div>
-        <div className="d-flex flex-column align-items-center w-50">
+        </motion.div>
+        <motion.div
+          initial={{ translateX: 0 }}
+          animate={animateCalloutRight}
+          className="d-flex flex-column align-items-center w-50"
+        >
           <TextRight delayWrapperRight={4} delayWord3={3} delayWord4={3.5} />
           <Companies delay={2} />
-        </div>
+        </motion.div>
       </div>
     </>
   );
